17ee50560017220ed404b1843f5d80aada6820e8
Advay Sharma <sharmaadvay0@gmail.com>
2020-12-17 15:14:08 -0800

    added photo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,21 @@
+import { Typography } from "@material-ui/core";
 import React from "react";
-
+import pfp from "../images/Portrait_Cropped.jpeg";
 const About = () => {
-  return <h1 className="text">About</h1>;
+  return (
+    <div className="about">
+      <Typography
+        style={{ color: "white", fontSize: 50, order: 1, paddingTop: "2.5%" }}
+      >
+        About Me
+      </Typography>
+      <img
+        src={pfp}
+        alt="pfp"
+        style={{ order: 2, width: "300px", paddingTop: "2.5%" }}
+      />
+    </div>
+  );
 };
 
 export default About;
