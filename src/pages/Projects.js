7369ccbb10707580d97c60a0e1cad9688e602c2d
Advay Sharma <sharmaadvay0@gmail.com>
2020-12-21 19:47:40 -0800

    added placeholder cards

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,70 @@
-import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import React, { Fragment } from "react";
 
 const Projects = () => {
-  return <h1 className="text">Projects</h1>;
+  return (
+    <Fragment>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "33%",
+            marginRight: "50px",
+          }}
+        >
+          <CardContent>
+            <Typography style={{ color: "white" }}>Project</Typography>
+          </CardContent>
+        </Card>
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "33%",
+          }}
+        >
+          <CardContent>
+            <Typography style={{ color: "white" }}>Project</Typography>
+          </CardContent>
+        </Card>
+      </div>
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10%",
+        }}
+      >
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "33%",
+            marginRight: "50px",
+          }}
+        >
+          <CardContent>
+            <Typography style={{ color: "white" }}>Project</Typography>
+          </CardContent>
+        </Card>
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "33%",
+          }}
+        >
+          <CardContent>
+            <Typography style={{ color: "white" }}>Project</Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Projects;
