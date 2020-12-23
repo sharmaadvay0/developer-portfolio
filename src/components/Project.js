5b23faf0184162bc502bcf39edca7b18a053b02e
Advay Sharma <52980112+sharmaadvay0@users.noreply.github.com>
2020-12-22 18:58:27 -0800

    Projects page (#4)
    * added placeholder cards
    
    * created projects component
    
    * added course planner project
    
    * cleanup and adds technologies used section in project

---
+++ src/components/Project.js
@@ -0,0 +1,107 @@
+import React, { Fragment } from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@material-ui/core";
+
+const Project = ({
+  title,
+  description,
+  githubLink,
+  imgPath,
+  imageFit,
+  techUsed,
+}) => {
+  return (
+    <Fragment>
+      {imageFit ? (
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "640px",
+          }}
+        >
+          <CardMedia
+            image={imgPath}
+            title={title}
+            style={{ height: "349px" }}
+          />
+          <CardContent>
+            <Typography style={{ color: "white", fontSize: 35 }}>
+              {title}
+            </Typography>
+            <Typography
+              style={{ color: "white", fontSize: 17, paddingTop: "2%" }}
+            >
+              {description}
+            </Typography>
+            <Typography
+              style={{ color: "white", fontSize: 17, paddingTop: "4%" }}
+            >
+              {techUsed}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginBottom: "2%", marginLeft: "1%" }}
+            >
+              <a
+                href={githubLink}
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                View On GitHub
+              </a>
+            </Button>
+          </CardActions>
+        </Card>
+      ) : (
+        <Card
+          style={{
+            backgroundColor: "#2a2a2a",
+            width: "320px",
+          }}
+        >
+          <CardMedia
+            image={imgPath}
+            title={title}
+            style={{ height: "174.5px" }}
+          />
+          <CardContent>
+            <Typography style={{ color: "white", fontSize: 35 }}>
+              {title}
+            </Typography>
+            <Typography
+              style={{ color: "white", fontSize: 17, paddingTop: "2%" }}
+            >
+              {description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button variant="contained" color="primary">
+              <a
+                href={githubLink}
+                style={{
+                  color: "white",
+                  textDecoration: "none",
+                }}
+              >
+                View On GitHub
+              </a>
+            </Button>
+          </CardActions>
+        </Card>
+      )}
+    </Fragment>
+  );
+};
+
+export default Project;
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,50 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useMediaQuery } from "react-responsive";
+import Project from "../components/Project";
 
 const Projects = () => {
-  return <h1 className="text">Projects</h1>;
+  const imageFit = useMediaQuery({ query: "(min-width: 700px)" });
+
+  //images should be 1280 × 698
+  //ideally, image format should be jpg
+  //images should be stored in public/images folder
+
+  return (
+    <Fragment>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "3%",
+        }}
+      >
+        <Project
+          title="Course Planner"
+          description="This project helps students create and store their college course plans. It includes an encrypted login and registration system, a dashboard for viewing saved plans, and an interface for creating and editing a plan. While editing a plan, the user can optionally add a fifth year and summer courses. The website also gives warnings or errors based on the number of units the student is taking per quarter."
+          githubLink="https://github.com/Course-Planner-UCSD/CoursePlanner"
+          imgPath="/images/coursePlanner.jpg"
+          techUsed="Used: JavaScript, MongoDB, Express, React, Node.js, Redux, Git"
+          imageFit={imageFit}
+        />
+      </div>
+      {/* <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "2.5%",
+        }}
+      >
+        <Project
+          title=""
+          description=""
+          githubLink=""
+          imgPath="/images/"
+          techUsed=""
+          imageFit={imageFit}
+        />
+      </div> */}
+    </Fragment>
+  );
 };
 
 export default Projects;
