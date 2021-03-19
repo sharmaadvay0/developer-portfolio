e9a3b16463f94101e3f0a68f4811579ef7fb7bd3
Advay Sharma <sharmaadvay0@gmail.com>
2021-03-18 18:14:21 -0700

    add community

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -19,27 +19,11 @@ const Projects = () => {
         }}
       >
         <Project
-          title="ACM Visualization"
-          description="ACM Visualization is a full-stack web application that shows event analytics for ACM, an organization within UC San Diego. This application displays a heatmap and line chart for event attendance, a bar graph for mean and median ACM points of attendees, and a pie chart for the distribution of events for ACM sub-organizations."
-          githubLink="https://github.com/acmucsd/sp-cerulean"
-          imgPath="/images/acm_visualization.png"
-          techUsed="Used: JavaScript, PostgreSQL, Express, React, Node.js, Git, Heroku"
-          imageFit={imageFit}
-        />
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "2.5%",
-        }}
-      >
-        <Project
-          title="Course Planner"
-          description="Course Planner is a full-stack web application that helps students create and store their college course plans. It includes an encrypted login and registration system, a dashboard for viewing saved plans, and an interface for creating and editing a plan. While editing a plan, the user can optionally add a fifth year and summer courses. The website also gives warnings or errors based on the number of units the student is taking per quarter."
-          githubLink="https://github.com/Course-Planner-UCSD/CoursePlanner"
-          imgPath="/images/coursePlanner.jpg"
-          techUsed="Used: JavaScript, MongoDB, Express, React, Node.js, Redux, Git"
+          title="Community"
+          description="Worked in a team of four on afull-stack web applicationthat enables users to add reports for issuespresent in their local community."
+          githubLink="https://github.com/Hackathon-Squad/Community"
+          imgPath="/images/community.png"
+          techUsed="JavaScript, Mapbox, Google Cloud Firestore, Google Cloud Storage, Express, React, Node.js"
           imageFit={imageFit}
         />
       </div>
@@ -52,11 +36,28 @@ const Projects = () => {
         }}
       >
         <Project
-          title="MCHS Events"
-          description="MCHS Events is an iOS app that displays Moreau Catholic High School's events. It displayed these events in a daily and weekly view."
-          githubLink=""
-          imgPath=""
-          techUsed="Used: Swift, PHP, JavaScript"
+          title="ACM Visualization"
+          description="ACM Visualization is a full-stack web application that shows event analytics for ACM, an organization within UC San Diego. This application displays a heatmap and line chart for event attendance, a bar graph for mean and median ACM points of attendees, and a pie chart for the distribution of events for ACM sub-organizations."
+          githubLink="https://github.com/acmucsd/sp-cerulean"
+          imgPath="/images/acm_visualization.png"
+          techUsed="JavaScript, PostgreSQL, Express, React, Node.js, Heroku"
+          imageFit={imageFit}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "2.5%",
+          marginBottom: "3%",
+        }}
+      >
+        <Project
+          title="Course Planner"
+          description="Course Planner is a full-stack web application that helps students create and store their college course plans. It includes an encrypted login and registration system, a dashboard for viewing saved plans, and an interface for creating and editing a plan. While editing a plan, the user can optionally add a fifth year and summer courses. The website also gives warnings or errors based on the number of units the student is taking per quarter."
+          githubLink="https://github.com/Course-Planner-UCSD/CoursePlanner"
+          imgPath="/images/coursePlanner.jpg"
+          techUsed="JavaScript, MongoDB, Express, React, Node.js, Redux"
           imageFit={imageFit}
         />
       </div>
